988996ee3ba53583195553c005c4ad531154dd77
andriusmw <andriusmw@yahoo.es>
2024-12-16 10:23:39 +0100

    routing + components

--- movie-omdb/src/app/app-routing.module.ts
+++ movie-omdb/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { DetailComponent } from './components/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/search', component: SearchComponent},
+  {path:'/detail', component: DetailComponent},
+  {path:'', pathMatch: 'full', redirectTo: 'search'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
