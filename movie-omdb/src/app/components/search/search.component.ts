d43a106262e1027654bb6d82c6d8fcb0806535b5
andriusmw <andriusmw@yahoo.es>
2024-12-16 11:51:16 +0100

    searching working

--- movie-omdb/src/app/components/search/search.component.ts
+++ movie-omdb/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OmdbService } from 'src/app/services/omdb.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  busqueda: string | undefined;
+
+constructor(private serviceOmdb: OmdbService) {}
+
+  buscar() {
+
+    this.serviceOmdb.getQuery(`&s=${this.busqueda}`)
+      .subscribe(response => {
+        console.log(response)
+      })
+  }
+
+
 
 }
